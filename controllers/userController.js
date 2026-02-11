af9fcf6fadfb561669bcfbdcfa68bbf05e3ebedd
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2026-02-11 14:05:06 +0700

    authorization

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 //@desc Register a new user
 //@route POST /api/users/register
-//@access public
+//@access private
 const registerUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
@@ -32,7 +32,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 //@desc Login user
 //@route POST /api/users/login
-//@access public
+//@access private
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
@@ -63,7 +63,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route GET /api/users/current
 //@access private
 const getCurrentUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Current user" });
+    res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, getCurrentUser };
