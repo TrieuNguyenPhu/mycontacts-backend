ef6415674ccac55b05cfe8054b8f655db0d4cf01
NguyenPhuTrieu <nguyentrieu080604@gmail.com>
2026-02-11 08:38:32 +0700

    jwt access token and uerlogin

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,62 @@
 const asyncHandler = require("express-async-handler");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 //@desc Register a new user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Register user" });
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are required");
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+    if (user) {
+        res.status(201).json(user);
+    } else {
+        res.status(400);
+        throw new Error("User not created");
+    }
 });
 
 //@desc Login user
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login user" });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are required");
+    }
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id,
+                },
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "15m" }
+        );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 //@desc Get current user
